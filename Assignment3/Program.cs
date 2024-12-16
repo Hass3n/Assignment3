e056c923a304f4da29688972346223784b93b710
Hassan <hassan.farahat@su.edu.eg>
2024-12-16 20:06:06 +0200

    Add project files.

---
+++ Assignment3/Program.cs
@@ -0,0 +1,215 @@
+﻿namespace Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Question(1)
+
+            //Console.WriteLine("Enter Your Number");
+            //int.TryParse(Console.ReadLine() , out int num);
+            //if(num%3 ==0 && num%4==0)
+            //    Console.WriteLine("yes");
+            //else
+            //    Console.WriteLine("No");
+
+            #endregion
+            #region Question(2)
+            //Console.WriteLine("Enter You Number");
+            //int.TryParse(Console.ReadLine(), out int number);
+            //if(number <0)
+            //    Console.WriteLine("Negative");
+            //else
+            //    Console.WriteLine("Positive");
+
+
+            #endregion
+            #region Question(3)
+            //Console.WriteLine("Enter Your First Number");
+            //int.TryParse(Console.ReadLine(), out int numberOne);
+            //Console.WriteLine("Enter Your Second Number");
+            //int.TryParse(Console.ReadLine(), out int numberTwo);
+            //Console.WriteLine("Enter Your Third Number");
+            //int.TryParse(Console.ReadLine(), out int numberThree);
+
+            //if (numberOne > numberTwo && numberOne > numberThree)
+            //{
+
+            //    Console.WriteLine("max element is " + numberOne);
+            //    if (numberTwo > numberThree)
+            //        Console.WriteLine("MIN number " + numberThree);
+            //    else
+            //        Console.WriteLine("MIN number " + numberTwo);
+
+            //}
+            //else if(numberTwo > numberOne && numberTwo > numberThree)
+            //{
+            //    Console.WriteLine("max element is " + numberTwo);
+            //    if (numberOne > numberThree)
+            //        Console.WriteLine("MIN number " + numberThree);
+            //    else
+            //        Console.WriteLine("MIN number " + numberOne);
+
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("max element is " + numberThree);
+            //    if (numberOne > numberTwo)
+            //        Console.WriteLine("MIN number " + numberTwo);
+            //    else
+            //        Console.WriteLine("MIN number " + numberOne);
+
+            //}
+
+
+
+
+            #endregion
+            #region Question(4)
+            //Console.WriteLine("Enter Your NUmber ");
+            //int.TryParse(Console.ReadLine(), out int num);
+            //if(num%2==0)
+            //    Console.WriteLine("even");
+            //else
+            //    Console.WriteLine("Odd");
+            #endregion
+            #region Question(5)
+            //Console.WriteLine("Enter Your Charecter");
+            //Char.TryParse(Console.ReadLine(), out char numChar);
+            //if(numChar=='a' || numChar=='e' || numChar =='i' || numChar=='o'|| numChar=='u')
+            //{
+
+            //    Console.WriteLine("Vowel");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Constant");
+            //}
+
+            #endregion
+            #region Question(6)
+
+            //Console.WriteLine("Enter Your number");
+            //int.TryParse(Console.ReadLine(), out int num);
+            //for (int i = 1; i <= 12; i++)
+            //{
+            //    Console.WriteLine($"{i}X{num} ="+i*num);
+
+            //}
+            #endregion
+            #region Question(7)
+
+            //Console.WriteLine("Enter Your number");
+            //int.TryParse(Console.ReadLine(), out int num);
+            //Console.Clear();
+
+
+            //for (int i = 1; i < num; i++)
+            //{
+
+            //    if(i%2==0)
+            //        Console.WriteLine(i);
+
+            //}
+            #endregion
+            #region Question(8)
+            //Console.WriteLine("Enter Your Base Number");
+            //int.TryParse(Console.ReadLine(), out int baseNumber);
+            //Console.WriteLine("Enter Your Power Number");
+            //int.TryParse(Console.ReadLine(), out int numPower);
+
+            //int result=1;
+            //for (int i = 1; i <= numPower; i++)
+            //{
+            //    result =result* baseNumber;  
+
+            //}
+
+            //Console.WriteLine("Power is" + result);
+
+
+            #endregion
+            #region Question(9)
+            //double  sum = 0.0;
+
+
+            //Console.WriteLine("Enter Your first Subject");
+            //int.TryParse(Console.ReadLine(), out int firstSubject);
+
+            //Console.WriteLine("Enter Your Second Subject");
+            //int.TryParse(Console.ReadLine(), out int secondSubject);
+
+            //Console.WriteLine("Enter Your Third Subject");
+            //int.TryParse(Console.ReadLine(), out int thirdSubject);
+
+            //Console.WriteLine("Enter Your fourSubject Subject");
+            //int.TryParse(Console.ReadLine(), out int fourSubject);
+
+            //Console.WriteLine("Enter Your fiveSubject Subject");
+            //int.TryParse(Console.ReadLine(), out int fiveSubject);
+
+            //sum = firstSubject + secondSubject + thirdSubject + fourSubject + fiveSubject;
+            //Console.WriteLine($"total Mark ={sum} ");
+            //Console.WriteLine($"average Mark is {sum/5}");
+            //Console.WriteLine($"The percantage is {sum/500*100}");
+            #endregion
+
+            #region Question(10)
+            //Console.WriteLine("Enter Number Of Month");
+            //int.TryParse(Console.ReadLine(), out int month);
+            //switch (month)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Days in month"+31);
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Days in month"+ 29);
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Days in month"+ 31);
+            //        break;
+
+            //    case 4:
+            //        Console.WriteLine("Days in month"+ 30);
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Days in month"+ 31);
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Days in month"+ 30);
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Days in month"+ 31);
+            //        break;
+            //    case 8:
+            //        Console.WriteLine("Days in month"+ 31);
+            //        break;
+            //    case 9:
+            //        Console.WriteLine("Days in month"+ 30);
+            //        break;
+            //    case 10:
+            //        Console.WriteLine("Days in month"+ 31);
+            //        break;
+            //    case 11:
+            //        Console.WriteLine("Days in month"+ 30);
+            //        break;
+            //    case 12:
+            //        Console.WriteLine("Days in month"+ 31);
+            //        break;
+            //    default:
+            //        Console.WriteLine("not found");
+            //        break;
+
+
+
+            //}
+
+            #endregion
+
+
+        }
+    }
+}
